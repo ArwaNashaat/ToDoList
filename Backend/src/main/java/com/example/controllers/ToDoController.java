e8df7a97ab6ad0c8d5219d432014852ddf0c5c56
Arwa Nashaat <arwanashaat@gmail.com>
2020-11-30 01:00:00 +0200

    rearrange files

--- Backend/src/main/java/com/example/controllers/ToDoController.java
+++ Backend/src/main/java/com/example/controllers/ToDoController.java
@@ -3,13 +3,13 @@ package com.example.controllers;
 import com.example.entities.ToDo;
 import com.example.repositories.ToDoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ToDoController {
     @Autowired
     ToDoRepository todoRepository;
-
     public ToDo addToDo(ToDo todo){
         return todoRepository.save(todo);
     }
